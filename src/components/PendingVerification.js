2e501f6ba671d4e76404c75c63177a8b6cdf5980
antonicarol <antonicarolgit@gmail.com>
2022-05-29 14:13:06 +0200

    Added verified start

---
+++ src/components/PendingVerification.js
@@ -0,0 +1,42 @@
+import { Accordion, Avatar, Button } from "flowbite-react";
+import React from "react";
+import { truncateWallet } from "../context/ethersUtils";
+
+export const PendingVerification = ({
+  key,
+  item,
+  openAcceptModal,
+  openDeclineModal,
+}) => {
+  return (
+    <Accordion.Panel className="">
+      <Accordion.Title arrowIcon={undefined}>
+        <div className="flex items-center gap-5">
+          <Avatar img={item.profileData.profileImg} />
+          <div>{item.profileData.username}</div>
+          <div>{truncateWallet(item.profileData.wallet)}</div>
+        </div>
+      </Accordion.Title>
+      <Accordion.Content>
+        <div className="border-b mb-5">
+          <p className="mb-2 text-gray-500 dark:text-gray-400">
+            {item.description}
+          </p>
+        </div>
+        <div className="flex flex-col items-end">
+          <div className="flex gap-8">
+            <Button onClick={openAcceptModal} className="items-end">
+              Accept Request
+            </Button>
+            <Button
+              onClick={openDeclineModal}
+              className="bg-red-600 hover:bg-red-800"
+            >
+              Decline Request
+            </Button>
+          </div>
+        </div>
+      </Accordion.Content>
+    </Accordion.Panel>
+  );
+};
--- src/pages/Artists.js
+++ src/pages/Artists.js
@@ -1,5 +1,52 @@
-import React from "react";
+import { Accordion, Avatar, Card } from "flowbite-react";
+import React, { useEffect, useState } from "react";
+import { PendingVerification } from "../components/PendingVerification";
+import marketplaceApi from "../context/axios";
 
 export const Artists = () => {
-  return <div>Artists</div>;
+  const [pendingRequests, setPendingRequests] = useState([]);
+  const [verifiedArtists, setVerifiedArtists] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const pending = await marketplaceApi.get("verify/allRequests");
+
+      setPendingRequests(pending.data);
+
+      const verified = await marketplaceApi.get("users/verified");
+      console.log(verified.data);
+      setVerifiedArtists(verified.data);
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <div className="flex">
+      <div className="flex flex-col gap-3 mx-10 my-5 p-4 w-full">
+        <div className="text-3xl uppercase"> Verify Requests </div>
+        {pendingRequests?.length > 0 ? (
+          <Accordion className="flex flex-col ">
+            {pendingRequests?.map((item) => {
+              return <PendingVerification key={item._id} item={item} />;
+            })}
+          </Accordion>
+        ) : (
+          <div>No Pending Suggestions</div>
+        )}
+      </div>
+      <div className="flex flex-col gap-3 mx-10 my-5 p-4 w-full">
+        <div className="text-3xl uppercase"> Verified Artists </div>
+        {verifiedArtists.map((item) => {
+          return (
+            <Card className="w-fit" key={item._id}>
+              <div className="flex items-center gap-3">
+                <Avatar img={item.profileImg} />
+                <div> {item.username}</div>
+              </div>
+            </Card>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
