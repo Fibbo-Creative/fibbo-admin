fdd0109f8c920976a5574d3e6f435211adba5a94
antonicarol <antonicarolgit@gmail.com>
2022-06-02 21:29:29 +0200

    Updated abis and address

--- src/constants/network.js
+++ src/constants/network.js
@@ -3,7 +3,7 @@ export const Contracts = {
     addressRegistry: "0x260ceD9e586082227Ec4513898CEc60E474063C5",
   },
   TESTNET: {
-    addressRegistry: "0x260ceD9e586082227Ec4513898CEc60E474063C5",
+    addressRegistry: "0x50dbc3b7551A18369F9Fd76E150Cc6BcF6df77F5",
   },
 };
 
